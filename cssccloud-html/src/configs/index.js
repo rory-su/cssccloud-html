0efa6de15458e88f0f30f579d2f9c359b3dc3160
rory-su <136471676@qq.com>
2019-10-21 18:27:47 +0800

    ffff

--- cssccloud-html/src/configs/index.js
+++ cssccloud-html/src/configs/index.js
@@ -1,1 +1,2 @@
-export const SERVICE_URL="http://192.168.50.23:8888";
+//export const SERVICE_URL="http://192.168.50.23:8888";
+export const SERVICE_URL="http://localhost:8888";
--- cssccloud-html/src/views/TenantPage/index.js
+++ cssccloud-html/src/views/TenantPage/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Button} from 'antd';
 import axios from 'axios';
 import * as configs from '../../configs';
+import myapi from '../../api';
+
 
 
 
@@ -14,7 +16,6 @@ export default class Tenant extends React.Component{
     }
     handelonclick=()=>{
     axios.post(configs.SERVICE_URL+"/loginaction?username=rory&password=123456").then(res=>{
-        console.log(res.data)
         this.setState({
             token:res.data.token
         })
@@ -23,17 +24,25 @@ export default class Tenant extends React.Component{
     })
     }
     ongetcode=()=>{
-        const {token}=this.state;
-        axios.get({url:configs.SERVICE_URL+"/oauth/authorize?client_id=client&response_type=code",header:{authorization:token}})
-              .then(res=>{
-                  console.log(res)
-              }).catch(error=>{console.log("fefe")})
+        // const {token}=this.state;
+        // axios({
+        //      method:"get",
+        //      url:configs.SERVICE_URL+"/user/list",
+        //      headers:{Authorization:"Bearer "+token}
+        //  }).then(res=>{
+        //           console.log(res)
+        //       }).catch(error=>{console.log("fefe")})
+       myapi.post("/loginaction?username=rory&password=123456").then(res=>{
+           console.log("请求结果",res)
+       }).catch(error=>{
+           console.log("请求出错",error)
+       })
     }
     render(){
         return<div>租户管理
-            <Button onClick={this.handelonclick}>获取token</Button>
+            <Button onClick={this.handelonclick}>登录获取token</Button>
             <div>token:{this.state.token}</div>
-            <Button onClick={this.ongetcode}>获取令牌</Button>
+            <Button onClick={this.ongetcode}>请求登录接口</Button>
         </div>
     }
 }
